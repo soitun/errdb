666c1b7f44fffcf3bcdcd16d67fc28534a95e321
huangdan <zzdhidden@gmail.com>
2012-04-25 19:29:38 +0800

    remove clients and add bin

--- clients/ruby/examples/basic.rb
+++
@@ -1,6 +0,0 @@
-require 'errdb'
-
-r = Errdb.new("localhost", 7272)
-
-r.fetch('key', 0, 9999999999)
-
--- src/errdb_ctl.erl
+++ src/errdb_ctl.erl
@@ -8,42 +8,17 @@
 
 -include("errdb.hrl").
 
--export([start/0,
-         process/1]).
+-compile(export_all).
 
-start() ->
-    case init:get_plain_arguments() of
-	[SNode | Args] ->
-	    SNode1 = case string:tokens(SNode, "@") of
-		[_Node, _Server] ->
-		    SNode;
-		_ ->
-		    case net_kernel:longnames() of
-			 true ->
-			     SNode ++ "@" ++ inet_db:gethostname() ++
-				      "." ++ inet_db:res_option(domain);
-			 false ->
-			     SNode ++ "@" ++ inet_db:gethostname();
-			 _ ->
-			     SNode
-		     end
-	    end,
-	    Node = list_to_atom(SNode1),
-	    Status = case rpc:call(Node, ?MODULE, process, [Args]) of
-			 {badrpc, Reason} ->
-			     ?PRINT("RPC failed on the node ~p: ~p~n",
-				       [Node, Reason]),
-			     ?STATUS_BADRPC;
-			 S ->
-			     S
-		     end,
-	    halt(Status);
-	_ ->
-	    print_usage(),
-	    halt(?STATUS_USAGE)
-    end.
+cluster(Node) ->
+	case net_adm:ping(list_to_atom(Node)) of
+	pong ->
+		?PRINT("cluster with ~p successfully.~n", [Node]);
+	pang ->
+        ?PRINT("failed to cluster with ~p~n", [Node])
+	end;
 
-process(["status"]) ->
+status() ->
     Infos = lists:flatten(errdb:info()),
     [?PRINT("process ~p: ~n~p~n", [Name, Info]) || {Name, Info} <- Infos],
     Tabs = ets:all(),
@@ -61,60 +36,8 @@ process(["status"]) ->
               [node(), InternalStatus, ProvidedStatus]),
     case lists:keysearch(errdb, 1, application:which_applications()) of
     false ->
-        ?PRINT("errdb is not running~n", []),
-        ?STATUS_ERROR;
+        ?PRINT("errdb is not running~n", []);
     {value, Version} ->
-        ?PRINT("errdb ~p is running~n", [Version]),
-        ?STATUS_SUCCESS
-    end;
-
-process(["cluster", Node]) ->
-	case net_adm:ping(list_to_atom(Node)) of
-	pong ->
-		?PRINT("cluster with ~p successfully.~n", [Node]),
-		?STATUS_SUCCESS;
-	pang ->
-        ?PRINT("failed to cluster with ~p~n", [Node]),
-        ?STATUS_ERROR
-	end;
-
-process(["stop"]) ->
-    init:stop(),
-    ?STATUS_SUCCESS;
-
-process(["restart"]) ->
-    init:restart(),
-    ?STATUS_SUCCESS;
-
-process(_Args) ->
-    print_usage(),
-    ?STATUS_USAGE.
-
-print_usage() ->
-    CmdDescs =
-	[{"status", "get errdb status"},
-	 {"stop", "stop errdb "},
-	 {"restart", "restart errdb"}],
-    MaxCmdLen =
-	lists:max(lists:map(
-		    fun({Cmd, _Desc}) ->
-			    length(Cmd)
-		    end, CmdDescs)),
-    NewLine = io_lib:format("~n", []),
-    FmtCmdDescs =
-	lists:map(
-	  fun({Cmd, Desc}) ->
-		  ["  ", Cmd, string:chars($\s, MaxCmdLen - length(Cmd) + 2),
-		   Desc, NewLine]
-	  end, CmdDescs),
-    ?PRINT(
-      "Usage: errdbctl [--node nodename] command [options]~n"
-      "~n"
-      "Available commands in this errdb node:~n"
-      ++ FmtCmdDescs ++
-      "~n"
-      "Examples:~n"
-      "  errdbctl stop~n"
-      "  errdbctl --node errdb@host restart~n",
-     []).
+        ?PRINT("errdb ~p is running~n", [Version])
+    end.
 
